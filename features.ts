68da9c4b92d613e788f894c820a90e1d5431d91b
NivethaSamraj <53819860+NivethaSamraj@users.noreply.github.com>
2026-06-20 15:33:03 +0530

    Create features.ts

---
+++ features.ts
@@ -0,0 +1,13 @@
+/**
+ * Simple data holder for account information used by feature preparations.
+ *
+ * Contains basic contact fields that can be populated by data generators
+ * or test fixtures.
+ */
+export class EnterAccountInformation {
+    firstName: string = '';
+    lastName: string = '';
+    companyName: string = '';
+}
+
+
